183f816ee4ae62df0277a93a130c1518218d6aab
mgalante <mgalante@opentrends.net>
2017-01-17 13:08:28 +0100

    add url links

--- app/src/main/java/com/example/mgalante/marvel_v20/views/showcharacter/ShowCharacter.java
+++ app/src/main/java/com/example/mgalante/marvel_v20/views/showcharacter/ShowCharacter.java
@@ -1,8 +1,11 @@
 package com.example.mgalante.marvel_v20.views.showcharacter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -11,8 +14,11 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.mgalante.marvel_v20.R;
 import com.example.mgalante.marvel_v20.api.entities.Characters;
+import com.example.mgalante.marvel_v20.api.entities.Url;
 import com.example.mgalante.marvel_v20.views.BaseActivity;
 import com.google.gson.Gson;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -58,7 +64,38 @@ public class ShowCharacter extends BaseActivity {
         mName.setText(mCharacter.getName());
         mDescription.setText(mCharacter.getDescription());
 
+        mComicLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 openUrl(mCharacter.getUrls(), "comiclink");
+            }
+        });
 
+        mDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUrl(mCharacter.getUrls(), "detail");
+            }
+        });
+
+        mWiki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUrl(mCharacter.getUrls(), "wiki");
+            }
+        });
+
+    }
+
+    private void openUrl(List<Url> urls, String type) {
+        for (Url url:urls
+                ) {
+            if (url.getType().equals(type)) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url.getUrl()));
+                startActivity(i);
+            }
+        }
     }
 
 
